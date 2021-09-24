031598d0f0a477c6289667685812d72def7dcd48
thiagoofdev <thiagoof.dev@gmail.com>
2021-09-24 09:01:22 -0300

    Modified of env

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -9,9 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['api-pontos-tur.herokuapp.com']
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+ALLOWED_HOSTS = ['api-pontos-tur.herokuapp.com']
 
 
 INSTALLED_APPS = [
